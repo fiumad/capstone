fa98ef31c705f10ee5e959a3c62ee8a3f4617cf7
Dan Fiumara <technodan143@gmail.com>
2024-03-10 22:28:15 -0400

    more epochs plus plotting

--- transimpedance_prediction/nn.py
+++ transimpedance_prediction/nn.py
@@ -2,10 +2,21 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader
 import data as SimulationData
+import matplotlib.pyplot as plt
 
 # device = "cuda" if torch.cuda.is_available() else "cpu"
 # torch.set_default_device(device)
 # print(f"Using {device} device")
+
+
+def plot_losses(loss_list):
+    plt.figure(figsize=(12, 6))
+    plt.plot(loss_list, label="Training Loss", marker=".")
+    plt.title("Training Loss")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.grid(True)
+    plt.show()
 
 
 class TransimpedanceModel(nn.Module):
@@ -27,12 +38,13 @@ if __name__ == "__main__":
     print("Data Spot Check:")
     dataset.print_data(50)
     dataloader = DataLoader(dataset, batch_size=16)  # , shuffle=True)
-    num_epochs = 1000
+    num_epochs = 2000
+    loss_list = []
 
     model = TransimpedanceModel()
     loss_fn = nn.MSELoss()
     learning_rate = 0.001556
-    new_low = 4000
+
     for epoch in range(num_epochs):
         optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
         for batch in dataloader:
@@ -42,18 +54,22 @@ if __name__ == "__main__":
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            learning_rate += 0.5e-5
-        print(f"Epoch {epoch+1}, Loss: {loss.item()}")
+            if loss.item() > 1000:
+                learning_rate += 0.5e-5
+        loss_list.append(loss.item())
+
         if loss.item() < 4000:
             learning_rate = learning_rate / 2
-        if loss.item() < 1000:
-            learning_rate = learning_rate / 2
+        print(f"Epoch {epoch+1}, Loss: {loss.item()}")
     # torch.save(model.state_dict(), "model.pth")
 
     input_data = (5e8, 1.84e-6)
     input_tensor = torch.tensor(input_data, dtype=torch.float)
 
+    # Plot the loss
+    plot_losses(loss_list[220:])
+
     model.eval()
     with torch.no_grad():
         output = model(input_tensor)
-        print(f"Input: {input_data}, Output: {output.item()}")
+        print(f"Input: {input_data}, Output: {output.item()}, Expected: 15000")
