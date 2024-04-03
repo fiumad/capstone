9d28a75040db2799a626385e387253a8c7918b61
Dan Fiumara <technodan143@gmail.com>
2024-04-03 00:02:00 -0400

    Begin training target approach

--- TargetApproach/TIModel.py
+++ TargetApproach/TIModel.py
@@ -1,12 +1,15 @@
 from torch import nn
 
+
 class TransimpedanceModel(nn.Module):
     def __init__(self):
         super(TransimpedanceModel, self).__init__()
         self.network = nn.Sequential(
-            nn.Linear(2, 64),
+            nn.Linear(3, 256),
             nn.ReLU(),
-            nn.Linear(64, 1),
+            nn.Linear(256, 256),
+            nn.ReLU(),
+            nn.Linear(256, 3),
         )
 
     def forward(self, x):
--- TargetApproach/nn.py
+++ TargetApproach/nn.py
@@ -50,61 +50,26 @@ if __name__ == "__main__":
     print("Data Spot Check:")
     dataset.print_data(50)
     dataloader = DataLoader(dataset, batch_size=32)  # , shuffle=True)
-    num_epochs = 355
+    num_epochs = 4000
     loss_list = []
 
     model = t.TransimpedanceModel()
     loss_fn = nn.MSELoss()
-    # learning_rate = 0.001556
-    learning_rate = 0.035
+    learning_rate = 0.0005
 
-    last_five = []
     for epoch in range(num_epochs):
         optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
         for batch in dataloader:
             inputs, targets = batch["inputs"], batch["targets"]
             predictions = model(inputs)
-            loss = loss_fn(predictions, targets.view(-1, 1))
+            loss = loss_fn(predictions, targets)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
         loss_list.append(loss.item())
-        # learning_rate = learning_rate + .0001
-        if loss.item() < 4000 and loss.item() > 3000:
-            learning_rate = learning_rate / 2
-        if loss.item() < 600 and loss.item() > 400:
-            learning_rate = learning_rate / 1.25
-        if loss.item() < 400 and loss.item() > 200:
-            learning_rate = learning_rate / 1.5
-        if loss.item() < 200 and loss.item() > 100:
-            learning_rate = learning_rate / 1.75
-        if loss.item() < 100 and learning_rate > 0.0002:
-            learning_rate -= learning_rate * 0.4
-        if loss.item() < 40 and loss.item() > 30:
-            learning_rate = learning_rate / 2
-        # if loss.item() < 4000:
-        # learning_rate = learning_rate / 2
         print(f"Epoch {epoch+1}, Loss: {loss.item()}, LR: {learning_rate}")
-        last_five.append(loss.item())
-        if len(last_five) > 5:
-            last_five.pop(0)
-        if len(last_five) == 5 and all(i < 40 for i in last_five):
-            print("Final Training Loss: ", loss.item())
-            break
 
-    input_data = (5e8, 1.84e-6)
-    input_tensor = torch.tensor(input_data, dtype=torch.float)
-
-    input_data1 = (100, 1.84e-6)
-    input_tensor1 = torch.tensor(input_data1, dtype=torch.float)
     # Plot the loss
     plot_losses(loss_list[10:])
-
-    model.eval()
-    with torch.no_grad():
-        output = model(input_tensor)
-        output1 = model(input_tensor1)
-        print(f"Input: {input_data}, Output: {output.item()}, Expected: ~ 15000")
-        print(f"Input: {input_data1}, Output: {output1.item()}, Expected: 14680")
 
     torch.save(model, "model.pth")
