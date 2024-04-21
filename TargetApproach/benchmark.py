e40b3d72df7068de63a6342ed6fff925bf4b0df2
Dan Fiumara <dan@fedora.danLaptop>
2024-04-21 15:19:36 -0400

    add scoring

--- TargetApproach/benchmark.py
+++ TargetApproach/benchmark.py
@@ -3,10 +3,11 @@ import nn
 from torch import nn
 import data
 import matplotlib.pyplot as plt
+import math
 
 
 def makePrediction(gain, bandwidth, power):
-    model = torch.load("./Checkpoints/4/model.pth")
+    model = torch.load("./Checkpoints/5/model.pth", map_location=torch.device("cpu"))
 
     model.eval()
 
@@ -18,6 +19,19 @@ def makePrediction(gain, bandwidth, power):
             f"Input: {input_data}, Input Width: {output[0].item() / 1000000}, Load Resistance: {output[1].item() * 10000}, Tail Width: {output[2].item() / 1000000}"
         )  # was output.item()
 
+def score(targetGain, targetBW, targetPower, actualGain, actualBW, actualPower):
+    print(f"Target Gain: {targetGain}, Target Bandwidth: {targetBW}, Target Power: {targetPower}")
+    print(f"Actual Gain: {actualGain}, Actual Bandwidth: {actualBW}, Actual Power: {actualPower}")
+    gainError = ((actualGain - targetGain) / targetGain) * 100
+    bwError = ( (math.log(actualBW/10**9, 10) - math.log(targetBW/10**9, 10)) / (math.log(targetBW/10**i9, 10)) ) * 100
+    powerError = ((targetPower - actualPower) / targetPower) * 100
+    print(f"Gain Error: {gainError}, Bandwidth Error: {bwError}, Power Error: {powerError}")
+
+    gainScore = 101 - (1 / (2.7182818 ** (0.091*gainError))) + (gainError/10)
+    bwScore = ((-1 * (bwError**2)) / 100) + 100
+    powerScore = 101 - (1 / (2.7182818 ** (0.153*powerError))) + (powerError/5)
+    
+    print(f"Gain Score: {gainScore}, Bandwidth Score: {bwScore}, Power Score: {powerScore}")
 
 def benchmark(width):
     CData = data.CircuitData()
@@ -77,3 +91,7 @@ if __name__ == "__main__":
         bandwidth = float(input("Enter Target Bandwidth (Hz): ")) / 10000000000
         power = float(input("Enter Target Tail Current (A): ")) * 100
         makePrediction(gain, bandwidth, power)
+        actualGain = float(input("Enter Actual Gain (V/A): "))
+        actualBW = float(input("Enter Actual Bandwidth (Hz): "))
+        actualPower = float(input("Enter Actual Tail Current (A): "))
+        score(gain * 10000, bandwidth*10000000000, power/100, actualGain, actualBW, actualPower)
