4fb5edc3f331a0635dc7a74562d83cff8a2fe731
Dan Fiumara <dan@fedora.danLaptop>
2024-04-23 14:22:38 -0400

    .

--- TargetApproach/TIModel.py
+++ TargetApproach/TIModel.py
@@ -5,11 +5,11 @@ class TransimpedanceModel(nn.Module):
     def __init__(self):
         super(TransimpedanceModel, self).__init__()
         self.network = nn.Sequential(
-            nn.Linear(3, 1024, bias=True),
+            nn.Linear(3, 256, bias=True),
             nn.ReLU(),
-            nn.Linear(1024, 1024, bias=True),
+            nn.Linear(256, 256, bias=True),
             nn.ReLU(),
-            nn.Linear(1024, 3, bias=True),
+            nn.Linear(256, 3, bias=True),
         )
 
     def forward(self, x):
--- TargetApproach/plotHist.py
+++ TargetApproach/plotHist.py
@@ -6,19 +6,21 @@ import numpy as np
 
 def plotHist(modelPath):
     all_weights = []
-    state_dict = torch.load(modelPath).state_dict()
+    state_dict = torch.load(modelPath, map_location=torch.device("cpu")).state_dict()
     for param_tensor in state_dict:
         if "weight" in param_tensor:
             weights = state_dict[param_tensor].cpu().numpy().flatten()
             all_weights.extend(weights)
 
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=(16, 12))
     plt.hist(all_weights, bins=100, color="blue")
-    plt.xlabel("Weight Value")
-    plt.ylabel("Frequency")
-    plt.title("Histogram of Weights")
+    plt.xlabel("Weight Value", fontsize=34)
+    plt.ylabel("Frequency", fontsize=36)
+    plt.title("Histogram of Weights", fontsize=36)
+    plt.yticks(fontsize=36)
+    plt.xticks(fontsize=36)
     plt.grid(True)
-    plt.show()
+    plt.savefig("WeightHistogram.png")
 
 
 if __name__ == "__main__":
